530fc28ecff6a5c2b231759790af5aaf75e5a8eb
albertmas <almaco_io@hotmail.es>
2018-03-20 12:32:48 +0100

    tsx added

---
+++ Project Shadow/Project Shadow/Assets/Map/sin_nombre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map2_spritesheet" tilewidth="32" tileheight="32" tilecount="1914" columns="58">
+ <image source="map2_spritesheet.png" width="1874" height="1070"/>
+</tileset>

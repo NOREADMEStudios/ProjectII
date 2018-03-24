62b23f41989337959d27b57711190b83c23c726d
Margeli <niniopantalones@gmail.com>
2018-03-24 19:11:12 +0100

    Added Module Fonts & ModuleGUI

--- Project Shadow/Project Shadow/Engine/ModuleGUI.cpp
+++ Project Shadow/Project Shadow/Engine/ModuleGUI.cpp
@@ -1,29 +1,29 @@
-#include "p2Defs.h"
-#include "p2Log.h"
-#include "j1App.h"
-#include "j1Render.h"
-#include "j1Textures.h"
+#include "Defs.h"
+#include "Log.h"
+#include "App.h"
+#include "ModuleRender.h"
+#include "ModuleTextures.h"
 #include "j1Fonts.h"
-#include "j1Input.h"
-#include "j1Gui.h"
-#include "j1Window.h"
-#include "Sprite.h"
-#include "Button.h"
-#include "Window.h"
-#include "Slider.h"
-#include "Brofiler\Brofiler.h"
+#include "ModuleInput.h"
+#include "ModuleGUI.h"
+#include "ModuleWindow.h"
+#include "UI\Sprite.h"
+#include "UI\Label.h"
+#include "UI\Window.h"
+#include "UI\Slider.h"
+#include "..\Brofiler\Brofiler.h"
 
-j1Gui::j1Gui() : j1Module()
+ModuleGUI::ModuleGUI() : Module()
 {
-	name.create("gui");
+	name ="gui";
 }
 
 // Destructor
-j1Gui::~j1Gui()
+ModuleGUI::~ModuleGUI()
 {}
 
 // Called before render is available
-bool j1Gui::Awake(pugi::xml_node& conf)
+bool ModuleGUI::Awake(pugi::xml_node& conf)
 {
 	LOG("Loading GUI atlas");
 	bool ret = true;
@@ -37,87 +37,84 @@ bool j1Gui::Awake(pugi::xml_node& conf)
 }
 
 // Called before the first frame
-bool j1Gui::Start()
+bool ModuleGUI::Start()
 {
 	return true;
 }
 
 // Update all guis
-bool j1Gui::PreUpdate()
+bool ModuleGUI::PreUpdate()
 {
-	BROFILER_CATEGORY(__FUNCTION__, Profiler::Color::DarkKhaki);
-	p2List_item<InterfaceElement*>* current_element = elements.end;
-	while (current_element != NULL)
-	{
-		current_element->data->enabled = current_element->data->next_frame_enabled;
-		if (current_element->data->isEnabled())
-			current_element->data->PreUpdate();
-		current_element = current_element->prev;
+	//BROFILER_CATEGORY(__FUNCTION__, Profiler::Color::DarkKhaki);
+	std::list<InterfaceElement*>::reverse_iterator current_element;
+	
+	for (current_element = elements.rend(); current_element != elements.rbegin(); current_element--) {
+		(*current_element)->enabled = (*current_element)->next_frame_enabled;
+		if ((*current_element)->isEnabled())
+			(*current_element)->PreUpdate();
 	}
-
 	/*if (focused_item != nullptr && (App->input->GetKey(SDL_SCANCODE_TAB) == KEY_DOWN || App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN))
 		focused_item->NextElement(focused_item)->setFocus();*/
 
 	return true;
 }
 
-bool j1Gui::Update(float dt)
+bool ModuleGUI::Update(float dt)
 {
-	BROFILER_CATEGORY(__FUNCTION__, Profiler::Color::DarkMagenta);
-	p2List_item<InterfaceElement*>* current_element = elements.start;
-	while (current_element != NULL)
-	{
-		if (current_element->data->isEnabled())
-			current_element->data->Update(dt);
-		current_element = current_element->next;
+	//BROFILER_CATEGORY(__FUNCTION__, Profiler::Color::DarkMagenta);
+	std::list<InterfaceElement*>::iterator current_element;
+
+	for (current_element = elements.begin(); current_element != elements.end(); current_element++) {
+		
+		if ((*current_element)->isEnabled())
+			(*current_element)->Update(dt);
 	}
 	return true;
 }
 
 // Called after all Updates
-bool j1Gui::PostUpdate()
+bool ModuleGUI::PostUpdate()
 {
-	p2List_item<InterfaceElement*>* current_element = elements.start;
-	while (current_element != NULL)
-	{
-		if (current_element->data->isEnabled())
-			current_element->data->PostUpdate();
-		current_element = current_element->next;
-	}
+	std::list<InterfaceElement*>::iterator current_element;
 
+	for (current_element = elements.begin(); current_element != elements.end(); current_element++) {
+		
+		if ((*current_element)->isEnabled())
+			(*current_element)->PostUpdate();
+	}
 	return true;
 }
 
 // Called before quitting
-bool j1Gui::CleanUp(pugi::xml_node&)
+bool ModuleGUI::CleanUp(pugi::xml_node& )
 {
 	LOG("Freeing GUI");
-	p2List_item<InterfaceElement*>* current_element = elements.end;
-	while (current_element != NULL)
-	{
-		current_element->data->CleanUp();
-		delete current_element->data;
-		current_element = current_element->prev;
+	std::list<InterfaceElement*>::reverse_iterator current_element;
+
+	for (current_element = elements.rend(); current_element != elements.rbegin(); current_element--) {
+		(*current_element)->enabled = (*current_element)->next_frame_enabled;
+		
+			(*current_element)->CleanUp();
 	}
 	elements.clear();
 
 	if(atlas_texture != nullptr)
-		App->tex->UnLoad(atlas_texture);
+		App->textures->UnLoad(atlas_texture);
 	return true;
 }
 
 // const getter for atlas
-const SDL_Texture* j1Gui::GetAtlas() const
+const SDL_Texture* ModuleGUI::GetAtlas() const
 {
 	return atlas_texture;
 }
 
-uiPoint j1Gui::GetGuiSize()
+uiPoint ModuleGUI::GetGuiSize()
 {
 	return gui_size;
 }
 
-void j1Gui::setFocus(InterfaceElement * elem)
+void ModuleGUI::setFocus(InterfaceElement * elem)
 {
 	if (focused_item != nullptr)
 		focused_item->in_focus = false;
@@ -126,36 +123,33 @@ void j1Gui::setFocus(InterfaceElement * elem)
 		focused_item->in_focus = true;
 }
 
-InterfaceElement * j1Gui::getFocusedItem()
+InterfaceElement * ModuleGUI::getFocusedItem()
 {
 	return focused_item;
 }
 
-InterfaceElement * j1Gui::AddElement(InterfaceElement * elem)
+InterfaceElement * ModuleGUI::AddElement(InterfaceElement * elem)
 {
-	if (elements.find(elem) < 0)
-		elements.add(elem);
+	elements.push_back(elem); // push back or front?
+
 
 	return elem;
 }
 
-bool j1Gui::RemoveElement(InterfaceElement * elem)
+void ModuleGUI::RemoveElement(InterfaceElement * elem)
 {
-	int index = elements.find(elem);
-	if (index > -1)
-		elements.del(elements.At(index));
-
-	return (index > -1);
+	elements.remove(elem);
+	
 }
 
-Sprite* j1Gui::AddSprite(int x, int y, SDL_Texture* tex, SDL_Rect anim, bool enabled)
+Sprite* ModuleGUI::AddSprite(int x, int y, SDL_Texture* tex, SDL_Rect anim, bool enabled)
 {
 	Sprite* aux = new Sprite(x, y, tex, anim, enabled);
-	elements.add(aux);
+	AddElement(aux);
 	return aux;
 }
 
-Label* j1Gui::AddLabel(int x, int y, int psize, const char * font_path, SDL_Color color, Label::RenderMode mode, const char* format, ...)
+Label* ModuleGUI::AddLabel(int x, int y, int psize, const char * font_path, SDL_Color color, Label::RenderMode mode, const char* format, ...)
 {
 	Label* aux = new Label(x, y, font_path, psize, mode);
 	aux->setColor(color);
@@ -174,43 +168,42 @@ Label* j1Gui::AddLabel(int x, int y, int psize, const char * font_path, SDL_Colo
 		}
 	}
 
-	elements.add(aux);
+	AddElement(aux);
 	return aux;
 }
 
-Button* j1Gui::AddButton(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback callback, SDL_Rect _hovered_anim, SDL_Rect _pressed_anim)
+Button* ModuleGUI::AddButton(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback_c callback, SDL_Rect _hovered_anim, SDL_Rect _pressed_anim)
 {
 	Button* aux = new Button(_x, _y, _tex, _anim, _enabled, callback, _hovered_anim, _pressed_anim);
 
-	elements.add(aux);
+	AddElement(aux);
 	return aux;
 }
 
-Slider* j1Gui::AddSlider(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback callback, SDL_Rect _hovered_anim, SDL_Rect _pressed_anim, bool _axis, InterfaceElement* parent)
+Slider* ModuleGUI::AddSlider(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback_c callback, SDL_Rect _hovered_anim, SDL_Rect _pressed_anim, bool _axis, InterfaceElement* parent)
 {
 	Slider* aux = new Slider(_x, _y, _tex, _anim, _enabled, callback, _hovered_anim, _pressed_anim, _axis);
 	
 	if (parent != NULL)
 		aux->SetParent(parent);
 	else
-		elements.add(aux);
-
+		AddElement(aux);
 	return aux;
 }
 
-Window* j1Gui::AddWindow(int x, int y, SDL_Texture* tex, SDL_Rect anim, bool enabled)
+Window* ModuleGUI::AddWindow(int x, int y, SDL_Texture* tex, SDL_Rect anim, bool enabled)
 {
 	Window* aux = new Window(x, y, tex, anim, enabled);
 
-	elements.add(aux);
+	AddElement(aux);
 	return aux;
 }
 
-Window * j1Gui::AddWindow(const Window_Info& info)
+Window * ModuleGUI::AddWindow(const Window_Info& info)
 {
 	Window* aux = new Window(info);
 
-	elements.add(aux);
+	AddElement(aux);
 	return aux;
 }
 // class Gui ---------------------------------------------------
--- Project Shadow/Project Shadow/Engine/ModuleGUI.h
+++ Project Shadow/Project Shadow/Engine/ModuleGUI.h
@@ -1,9 +1,10 @@
-#ifndef __j1GUI_H__
-#define __j1GUI_H__
+#ifndef __MODULEGUI_H__
+#define __MODULEGUI_H__
 
-#include "j1Module.h"
-#include "InterfaceElement.h"
-#include "Label.h"
+#include "Module.h"
+#include "UI\InterfaceElement.h"
+#include "UI\Label.h"
+#include <list>
 
 class Sprite;
 class Label;
@@ -13,33 +14,34 @@ class Slider;
 struct Window_Info;
 
 #define CURSOR_WIDTH 2
+#define TMP_STRING_SIZE 4096
 
 // TODO 1: Create your structure of classes
 
 // ---------------------------------------------------
-class j1Gui : public j1Module
+class ModuleGUI : public Module
 {
 public:
 
-	j1Gui();
+	ModuleGUI();
 
 	// Destructor
-	virtual ~j1Gui();
+	virtual ~ModuleGUI();
 
 	// Called when before render is available
-	bool Awake(pugi::xml_node&);
+	bool Awake(pugi::xml_node&) override;
 
 	// Call before first frame
-	bool Start();
+	bool Start() override;
 
 	// Called before all Updates
-	bool PreUpdate();
+	bool PreUpdate() override;
 
 	// Called every frame
 	bool Update(float dt) override;
 
 	// Called after all Updates
-	bool PostUpdate();
+	bool PostUpdate() override;
 
 	// Called before quitting
 	bool CleanUp(pugi::xml_node&) override;
@@ -48,21 +50,21 @@ public:
 	// Gui creation functions
 	Sprite* AddSprite(int x, int y, SDL_Texture* tex, SDL_Rect anim = { 0, 0, 0, 0 }, bool enabled = true);
 	Label* AddLabel(int x, int y, int psize, const char* font_path = nullptr, SDL_Color color = {0,0,0,255}, Label::RenderMode mode = Label::BLENDED, const char* format = nullptr, ...);
-	Button* AddButton(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback callback, SDL_Rect _hovered_anim = { 0, 0, 0, 0 }, SDL_Rect _pressed_anim = { 0, 0, 0, 0 });
+	Button* AddButton(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback_c callback, SDL_Rect _hovered_anim = { 0, 0, 0, 0 }, SDL_Rect _pressed_anim = { 0, 0, 0, 0 });
 	Window* AddWindow(int x, int y, SDL_Texture* tex, SDL_Rect anim, bool enabled = true);
 	Window* AddWindow(const Window_Info& info);
-	Slider* AddSlider(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback callback, SDL_Rect _hovered_anim = { 0, 0, 0, 0 }, SDL_Rect _pressed_anim = { 0, 0, 0, 0 }, bool _axis = 1, InterfaceElement* parent = nullptr);
+	Slider* AddSlider(int _x, int _y, SDL_Texture* _tex, SDL_Rect _anim, bool _enabled, Callback_c callback, SDL_Rect _hovered_anim = { 0, 0, 0, 0 }, SDL_Rect _pressed_anim = { 0, 0, 0, 0 }, bool _axis = 1, InterfaceElement* parent = nullptr);
 	const SDL_Texture* GetAtlas() const;
-	p2SString atlas_file_name;
+	std::string atlas_file_name;
 
 	uiPoint GetGuiSize();
 	void setFocus(InterfaceElement* elem);
 	InterfaceElement* getFocusedItem();
 
 	InterfaceElement* AddElement(InterfaceElement* elem);
-	bool RemoveElement(InterfaceElement* elem);
+	void RemoveElement(InterfaceElement* elem);
 
-	p2List<InterfaceElement*> elements;
+	std::list<InterfaceElement*> elements;
 
 private:
 	SDL_Texture* atlas_texture = nullptr;

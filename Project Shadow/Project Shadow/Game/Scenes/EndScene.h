881b7d391277f8bead3161dea5741040bb5091d4
Normanbg <normanbg98@gmail.com>
2018-04-05 14:28:46 +0200

    EndScene update

--- Project Shadow/Project Shadow/Game/Scenes/EndScene.h
+++ Project Shadow/Project Shadow/Game/Scenes/EndScene.h
@@ -1,8 +1,34 @@
-#pragma once
-class EndScene
+#ifndef __ENDSCENE_H__
+#define __ENDSCENE_H__
+
+#include "..\..\Engine\Scene.h"
+#include "..\..\Engine\ModuleCollision.h"
+
+class EndScene :
+	public Scene
 {
 public:
+
+
 	EndScene();
-	~EndScene();
+	virtual ~EndScene();
+
+	bool Awake(pugi::xml_node&) override { return true; };
+
+	bool Start() override { return true; };
+
+	bool PreUpdate() override { return true; }
+
+	bool Update(float dt) override { return true; };
+
+	bool PostUpdate() override { return true; }
+
+	bool CleanUp(pugi::xml_node&) override { return true; };
+
+	bool Load(pugi::xml_node&) override { return true; };
+
+	bool Save(pugi::xml_node&) const override { return true; };
+
 };
 
+#endif

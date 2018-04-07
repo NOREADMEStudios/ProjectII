91b33f09cbb0b2fdbb652ebcab44affd11f8bc64
Marc <marcmoreno2@gmail.com>
2018-04-07 05:50:09 +0200

    Scale no longer affects UI or mouse input

--- Project Shadow/Project Shadow/Engine/ModuleRender.cpp
+++ Project Shadow/Project Shadow/Engine/ModuleRender.cpp
@@ -277,7 +277,7 @@ bool ModuleRender::DrawLine(int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Ui
 	if(use_camera)
 		result = SDL_RenderDrawLine(renderer, camera.x + x1 * scale, camera.y + y1 * scale, camera.x + x2 * scale, camera.y + y2 * scale);
 	else
-		result = SDL_RenderDrawLine(renderer, x1 * scale, y1 * scale, x2 * scale, y2 * scale);
+		result = SDL_RenderDrawLine(renderer, x1 /** scale*/, y1 /** scale*/, x2 /** scale*/, y2 /** scale*/);
 
 	if(result != 0)
 	{

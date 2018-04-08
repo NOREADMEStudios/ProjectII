e05265c145884a3b03722f259a4f7fc0cd912687
Normanbg <normanbg98@gmail.com>
2018-04-08 14:07:40 +0200

    Revert "Item selec changes"
    This reverts commit cf767c9639b1fe5f26a47565742cf2f6f18677ec.

--- Project Shadow/Project Shadow/Game/Scenes/ItemSelecScene.h
+++ Project Shadow/Project Shadow/Game/Scenes/ItemSelecScene.h
@@ -8,27 +8,27 @@ class ItemSelecScene:
 	public Scene
 {
 public:
-	
-	Button *item1, *item2, *item3;
+
 
 	ItemSelecScene();
 	virtual ~ItemSelecScene();
 
 	bool Awake(pugi::xml_node&) override { return true; };
 
-	bool Start() override;
+	bool Start() override { return true; };
 
 	bool PreUpdate() override { return true; }
 
-	bool Update(float dt) override;
+	bool Update(float dt) override { return true; };
 
 	bool PostUpdate() override { return true; }
 
-	bool CleanUp() override;
+	bool CleanUp(pugi::xml_node&) override { return true; };
 
 	bool Load(pugi::xml_node&) override { return true; };
 
 	bool Save(pugi::xml_node&) const override { return true; };
+
 
 };
 

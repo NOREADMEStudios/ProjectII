7ee2f23852b4135f7ef709948603b3821167d983
Normanbg <normanbg98@gmail.com>
2018-04-08 14:10:10 +0200

    Itemselec start

--- Project Shadow/Project Shadow/Game/Scenes/ItemSelecScene.cpp
+++ Project Shadow/Project Shadow/Game/Scenes/ItemSelecScene.cpp
@@ -1,12 +1,43 @@
 #include "ItemSelecScene.h"
-
+#include "EndScene.h"
+#include "IntroScene.h"
+#include "MainScene.h"
+#include "../../Engine/ModuleMap.h"
+#include "../../Engine/App.h"
+#include "../../Engine/ModuleSceneManager.h"
+#include "../../Engine/ModuleEntityManager.h"
+#include "../../Engine/ModuleGUI.h"
+#include "../../Engine/ModuleTextures.h"
+#include "../../Engine/ModuleInput.h"
+#include "../../Engine/ModuleRender.h"
+#include "../../Engine/UI/Window.h"
+#include "../../Engine/ModuleCollision.h"
 
 
 ItemSelecScene::ItemSelecScene()
 {
 }
 
-
 ItemSelecScene::~ItemSelecScene()
 {
 }
+
+bool ItemSelecScene::Start()
+{
+	return false;
+}
+
+bool ItemSelecScene::Update(float dt)
+{
+	if (App->input->GetKey(SDL_SCANCODE_0) == KEY_DOWN) {
+		App->scenes->ChangeScene(App->scenes->introSc);
+	}
+
+	return true;
+}
+
+bool ItemSelecScene::CleanUp()
+{
+	return true;
+}
+

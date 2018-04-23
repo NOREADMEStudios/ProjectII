d7481493a8b7d898fa3f30b81c3ac5522b851bff
Margeli <niniopantalones@gmail.com>
2018-04-23 10:55:15 +0200

    Created classes

---
+++ Project Shadow/Project Shadow/Engine/Spells.cpp
@@ -0,0 +1,12 @@
+#include "Spells.h"
+
+
+
+Spells::Spells()
+{
+}
+
+
+Spells::~Spells()
+{
+}
---
+++ Project Shadow/Project Shadow/Engine/Spells.h
@@ -0,0 +1,8 @@
+#include "Entity.h"
+class Spells : 	public Entity
+{
+public:
+	Spells();
+	~Spells();
+};
+

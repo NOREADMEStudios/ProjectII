6f06d44bad9499c5a941a89cb16f38bc77dadf2f
PolCarCat <polcarcat@gmail.com>
2018-05-07 10:12:04 +0200

    Starting wizard

--- Project Shadow/Project Shadow/Engine/Wizard.cpp
+++ Project Shadow/Project Shadow/Engine/Wizard.cpp
@@ -26,8 +26,15 @@ bool Wizard::Awake(pugi::xml_node&)
 
 bool Wizard::HeroStart()
 {
-
+	Attack* light_1 = new Attack(1, LIGHT_ATTACK, "attack_1", animations_name, 2);
+	Attack* heavy_1 = new Attack(2, HEAVY_ATTACK, "attack_dagger", animations_name, 2);
+	Attack* jump_a = new Attack(3, JUMPINPUT, "jump", animations_name, 0, true);
 	
+
+	attacks.push_back(light_1);
+	attacks.push_back(heavy_1);
+	attacks.push_back(jump_a);
+
 	return true;
 }
 
@@ -40,6 +47,15 @@ bool Wizard::PreUpdate()
 
 bool Wizard::HeroUpdate(float dt)
 {
+
+	if (directions.right - directions.left == 1)
+	{
+		flip = false;
+	}
+	else if (directions.right - directions.left == -1)
+	{
+		flip = true;
+	}
 
 	return true;
 }

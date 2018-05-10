fea2f096358adb938b63433d9fd52f0defd20490
PolCarCat <polcarcat@gmail.com>
2018-05-10 21:46:02 +0200

    Elf bug

--- Project Shadow/Project Shadow/Engine/Cleric.cpp
+++ Project Shadow/Project Shadow/Engine/Cleric.cpp
@@ -21,12 +21,41 @@ Cleric::~Cleric()
 
 bool Cleric::Awake(pugi::xml_node&)
 {
+
+
 	return true;
 }
 
 bool Cleric::HeroStart()
 {
+	Attack* light_1 = new Attack(1, LIGHT_ATTACK, "attack", animations_name, 1);
+	Attack* heavy_1 = new Attack(2, HEAVY_ATTACK, "attack_2", animations_name, 5);
+	Attack* crouch = new Attack(4, LIGHT_ATTACK, "attack_3", animations_name, 1);
+	Attack* jump_a = new Attack(3, JUMPINPUT, "jump", animations_name, 0, true);
+	Attack* jump_a2 = new Attack(5, LIGHT_ATTACK, "attack_j1", animations_name, 0, true);
+	Attack* ab_1 = new Attack(11, AB_1, "attack_m1", animations_name, 0, false, true);
+	Attack* ab_2 = new Attack(12, AB_2, "attack_m2", animations_name, 0, false, true);
+	Attack* ab_3 = new Attack(13, AB_3, "win", animations_name, 0, false, true);
 
+	attacks.push_back(light_1);
+	attacks.push_back(heavy_1);
+	attacks.push_back(crouch);
+	attacks.push_back(jump_a);
+	attacks.push_back(ab_1);
+	attacks.push_back(ab_2);
+	attacks.push_back(jump_a2);
+	attacks.push_back(ab_3);
+
+	light_1->AddChild(crouch);
+	jump_a->AddChild(jump_a2);
+
+	Ability* fire = new Ability(ab_1, 3);
+	Ability* thunder = new Ability(ab_2, 5);
+	Ability* ulti = new Ability(ab_3, 10);
+
+	AdAbility(*fire);
+	AdAbility(*thunder);
+	AdAbility(*ulti);
 
 	return true;
 }
@@ -132,7 +161,7 @@ void Cleric::OnCollisionEnter(Collider* _this, Collider* _other)
 		}
 		else if (_this->type == Collider::ATK && _other->type == Collider::HITBOX && StateisAtk(currentState))
 		{
-			Attack * atk = GetAtk(currentState);
+			Attack * atk = GetAtk(currentTag);
 			if (atk != nullptr)
 				_other->entity->stats.life -= _this->entity->stats.atk + atk->damage - _other->entity->stats.def;
 
